df7b54c476a324f6ded5924882073f00247936a2
Ken <kenrgoss@gmail.com>
2016-06-29 12:40:37 -0500

    Created Mission Division algorithm

--- Ken/rectPlan.py
+++ Ken/rectPlan.py
@@ -8,10 +8,10 @@ from math import pi
 
 class latlon:
     n=float()
-    w=float()
-    def __init__(self, north, west):
+    e=float()
+    def __init__(self, north, east):
         self.n=north
-        self.w=west
+        self.e=east
 
 class mission:
     latitude=float()
@@ -53,7 +53,7 @@ The algorithm assumes the orientation
     f2 is the field just north of that one
     f3 is a field on a strange angle I found for testing'''
 
-f1=[latlon(38.893866,-92.201769),latlon(38.893865, -92.201024),latlon(38.894552,-92.201016),latlon(38.894554,-92.201748)]
+f1=[latlon(38.893866,-92.201769),latlon(38.893865, -92.201024),latlon(38.894552,-92.201016),latlon(38.894554,-92.201748), latlon(38.894035, -92.200937), latlon(38.894281, -92.200959) ]
 f2=[latlon(38.894687,-92.202445),latlon(38.894659,-92.201019),latlon(38.895398,-92.201018),latlon(38.895424,-92.202389)]
 f3=[latlon(38.202472,-91.736857),latlon(38.203990,-91.734097),latlon(38.205077,-91.735023),latlon(38.203475,-91.737941)]
 
@@ -61,26 +61,26 @@ badPoints=[latlon(38.893968,-92.201754),latlon(38.893906, -92.201759),latlon(38.
 badPoints2=[latlon(38.893866,-92.201769), latlon(38.893964, -92.201031), latlon(38.894561, -92.200889)]
 def add(p1, p2):
     north=p1.n+p2.n
-    west=p1.w+p2.w
+    west=p1.e+p2.e
     return latlon(north,west)
 
 def sub(p1,p2):
     north=p1.n-p2.n
-    west=p1.w-p2.w
+    west=p1.e-p2.e
     return latlon(north,west)
 
 def sdiv(p,scalar):
     north=p.n/scalar
-    west=p.w/scalar
+    west=p.e/scalar
     return latlon(north,west)
 
 def smult(p, scalar):
     north=p.n*scalar
-    west=p.w*scalar
+    west=p.e*scalar
     return latlon(north,west)
 
 def mag(p):
-    return (p.n*p.n+p.w*p.w)**.5
+    return (p.n*p.n+p.e*p.e)**.5
 
 def isPerpendicular(p1,p2,p3):
     tightTolerance=.04
@@ -99,7 +99,7 @@ def isPerpendicular(p1,p2,p3):
 
 
 def rectMission(p1, p2, p3, alt, cam='gopro', imgOvr=.05):
-    picList=list()
+    rectSurvey={'response':'bad', 'picList':list()}
     camParam={'pi':{'ssizem':2.74, 'ssizep':3.76, 'flen':3.6, 'angN' : 0.7272647522337332, 'angW' : 0.9625338617968637, 'TangN':.8900036993, 'TangW':1.436087493},
               'canon':{'ssizem':5.7, 'ssizep':7.6, 'flen':5.2, 'angN' : 1.0027311229353408, 'angW' : 1.2621587749426584, 'TangN':1.566803225, 'TangW':3.1365079},
               'gopro':{'angN':2.792523803, 'angW':2.792523803, 'TangN':2.3857296493600746, 'TangW':2.3857296493600746}}
@@ -107,16 +107,16 @@ def rectMission(p1, p2, p3, alt, cam='gopro', imgOvr=.05):
     if perpendicularTestResult=="Good" or perpendicularTestResult=="Warn":
         v21=sub(p1,p2)
         v23=sub(p3,p2)
-        picList.append(perpendicularTestResult)
+        rectSurvey['response']=perpendicularTestResult
 
-        vectorAngle=atan(v21.n/v21.w)*180/pi
+        vectorAngle=atan(v21.n/v21.e)*180/pi
         if v21.n<0:
-            if v21.w<0:
+            if v21.e<0:
                 bearing=270-abs(vectorAngle) #quadrant 3
             else:
                 bearing=90+abs(vectorAngle) #quadrant 2
         else:
-            if v21.w<0:
+            if v21.e<0:
                 bearing = 270+abs(vectorAngle) #quadrant 4
             else:
                 bearing = 90-abs(vectorAngle) #quadrant 1
@@ -130,39 +130,86 @@ def rectMission(p1, p2, p3, alt, cam='gopro', imgOvr=.05):
         picNum=0
         position=add(add(p2,sdiv(outerstep,2)),sdiv(innerstep,2))
         picNum+=1
-        picList.append(mission(position.n,position.w,alt,bearing,picNum))
-        #print (position.n, position.w)
-        #print (str(position.n)+','+str(position.w))
+        rectSurvey['picList'].append(mission(position.n,position.e,alt,bearing,picNum))
+        print("pic append worked")
+        #print (position.n, position.e)
+        #print (str(position.n)+','+str(position.e))
         for i in range(0,int(outerlimit+1)):
             for k in range(0,int(innerlimit-1)):
                 if i%2==0:
                     position=add(position,innerstep)
-                    #print (str(position.n)+','+str(position.w))
+                    #print (str(position.n)+','+str(position.e))
                 else:
                     position=sub(position,innerstep)
-                    #print (str(position.n)+','+str(position.w))
+                    #print (str(position.n)+','+str(position.e))
 
                 picNum+=1
-                picList.append(mission(position.n,position.w,alt,bearing,picNum))
+                rectSurvey['picList'].append(mission(position.n,position.e,alt,bearing,picNum))
             if i!= outerlimit:
                 position=add(position,outerstep)
-                #print (str(position.n)+','+str(position.w))
+                #print (str(position.n)+','+str(position.e))
                 picNum+=1
-                picList.append(mission(position.n,position.w,alt,bearing,picNum))
-        #print (str(position.n)+','+str(position.w), picNum)
-        return picList
+                rectSurvey['picList'].append(mission(position.n,position.e,alt,bearing,picNum))
+        #print (str(position.n)+','+str(position.e), picNum)
+        return rectSurvey
     else:
         print ("Error: Given points do not form a sufficiently perpindiucalr angle for optimal operation")
         return perpendicularTestResult
-missionList=rectMission(f1[0],f1[1],f1[2],20, 'pi')
+
+
+def missionDivision(pointList, droneList):
+    print("In missionDivision")
+    dividedMission={'response':pointList['response'], 'locations':list()}
+    print("In missionDivision precondition")
+    if len(droneList)==3:
+        print("Only one drone")
+        dividedMission['locations'].append({'name':dronelist[0], 'points':pointList['picList']})
+        return dividedMission
+
+    elif len(droneList)==6:
+        print("two drones")
+        drone1location=latlon(droneList[1],droneList[2])
+        drone2location=latlon(droneList[4],droneList[5])
+        numPoints=len(pointList['picList'])
+        print(numPoints)
+        halfIndex=int(numPoints/2)
+        distance4d1begd2half=mag(sub(drone1location,latlon(pointList['picList'][0].latitude, pointList['picList'][0].longitude)))+mag(sub(drone2location,latlon(pointList['picList'][halfIndex].latitude, pointList['picList'][halfIndex].longitude)))
+        distance4d2begd1half=mag(sub(drone2location,latlon(pointList['picList'][0].latitude, pointList['picList'][0].longitude)))+mag(sub(drone1location,latlon(pointList['picList'][halfIndex].latitude, pointList['picList'][halfIndex].longitude)))
+        if distance4d1begd2half <= distance4d2begd1half:
+            dividedMission['locations'].append({'name':droneList[0], 'points':pointList['picList'][0:halfIndex]})
+            print(droneList[0])
+            dividedMission['locations'].append({'name':droneList[3], 'points':pointList['picList'][halfIndex:numPoints+1]})
+            print(droneList[3])
+            print('d1 first d2 half')
+        else:
+            dividedMission['locations'].append({'name':droneList[3], 'points':pointList['picList'][0:halfIndex]})
+            dividedMission['locations'].append({'name':droneList[0], 'points':pointList['picList'][halfIndex+1:numPoints+1]})
+            print('d2 first d1 half')
+        return dividedMission
+    else:
+        print("Wrong")
+        return
+
+
+surveyPlan=rectMission(f1[0],f1[1],f1[2],20, 'pi')
 #missionList=rectMission(f2[2],f2[3],f2[0],20, 'canon')
 #missionList=rectMission(f3[2],f3[3],f3[0],20, 'pi')
 #missionList=rectMission(f1[0],f1[1],badPoints[2],20, 'pi')
-if missionList != None:
-    for x in missionList :
-        if x == missionList[0]:
-            print(x)
-        elif x!=missionList[-1]:
+
+if surveyPlan != None:
+    print(surveyPlan['response'])
+    for x in surveyPlan['picList'] :
+        if x!=surveyPlan['picList'][-1]:
             print(x.latitude,',',x.longitude)
         else:
             print(x.latitude,',',x.longitude,x.bearing,x.ordLoc)
+
+dlist=["gold", f1[4].n, f1[4].e, "green", f1[5].n, f1[5].e]
+
+data=missionDivision(surveyPlan, dlist)
+print('missionDivision worked')
+print()
+for location in data['locations']:
+    print(location['name'])
+    for point in location['points']:
+        print(point.latitude, point.longitude)
