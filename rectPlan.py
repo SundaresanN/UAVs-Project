d127292e9a3cd52c921468c8a51c056136f148cb
Ken <kenrgoss@gmail.com>
2016-07-01 09:14:17 -0500

    fixed division index error

--- rectPlan.py
+++ rectPlan.py
@@ -163,6 +163,8 @@ def rectMission(p1, p2, p3, alt, cam='gopro', imgOvr=.05):
 
 def missionDivision(pointList, droneList):
     dividedMission={'response':pointList['response'], 'UAVs':list()}
+    picListLength=len(pointList['picList'])
+    picListMidpoint=int(picListLength/2)
     if len(droneList)==3:
         dividedMission['UAVs'].append({'name':dronelist[0], 'points':pointList['picList']})
         return dividedMission
@@ -171,15 +173,15 @@ def missionDivision(pointList, droneList):
         drone1location=latlon(droneList[1],droneList[2])
         drone2location=latlon(droneList[4],droneList[5])
         numPoints=len(pointList['picList'])
-        halfIndex=int(numPoints/2)
+
         distance4d1begd2half=mag(sub(drone1location,latlon(pointList['picList'][0].latitude, pointList['picList'][0].longitude)))+mag(sub(drone2location,latlon(pointList['picList'][halfIndex].latitude, pointList['picList'][halfIndex].longitude)))
         distance4d2begd1half=mag(sub(drone2location,latlon(pointList['picList'][0].latitude, pointList['picList'][0].longitude)))+mag(sub(drone1location,latlon(pointList['picList'][halfIndex].latitude, pointList['picList'][halfIndex].longitude)))
         if distance4d1begd2half <= distance4d2begd1half:
-            dividedMission['UAVs'].append({'name':droneList[0], 'points':pointList['picList'][0:halfIndex]})
-            dividedMission['UAVs'].append({'name':droneList[3], 'points':pointList['picList'][halfIndex:numPoints+1]})
+            dividedMission['UAVs'].append({'name':droneList[0], 'points':pointList['picList'][0:picListMidpoint]})
+            dividedMission['UAVs'].append({'name':droneList[3], 'points':pointList['picList'][picListMidpoint:picListLength]})
         else:
-            dividedMission['UAVs'].append({'name':droneList[3], 'points':pointList['picList'][0:halfIndex]})
-            dividedMission['UAVs'].append({'name':droneList[0], 'points':pointList['picList'][halfIndex+1:numPoints+1]})
+            dividedMission['UAVs'].append({'name':droneList[3], 'points':pointList['picList'][0:picListMidpoint]})
+            dividedMission['UAVs'].append({'name':droneList[0], 'points':pointList['picList'][picListMidpoint:picListLength]})
         return dividedMission
     else:
         print("Wrong")
@@ -198,13 +200,13 @@ def serializeMissionData(missionDivisionData):
                     "altitude" : missionDivisionData['UAVs'][indexLocations]['points'][indexPoints].altitude,
             }
     return missionDivisionData
-    
+
 #surveyPlan=rectMission(f1[0],f1[1],f1[2],20, 'pi')
 #missionList=rectMission(f2[2],f2[3],f2[0],20, 'canon')
 #missionList=rectMission(f3[2],f3[3],f3[0],20, 'pi')
 #missionList=rectMission(f1[0],f1[1],badPoints[2],20, 'pi')
-'''
-if surveyPlan != None:
+
+'''if surveyPlan != None:
     print(surveyPlan['response'])
     for x in surveyPlan['picList'] :
         if x!=surveyPlan['picList'][-1]:
